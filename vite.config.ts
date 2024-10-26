21a99f62f1a800b1295b3843a755b2582d484d03
趙子賢 <danny900714@gmail.com>
2024-10-26 15:25:34 +0800

    Cleanup project and update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,21 @@ import {
 } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { type PlatformProxy } from "wrangler";
+
+// PlatformProxy’s caches property is incompatible with the caches global
+// https://github.com/cloudflare/workers-sdk/blob/main/packages/wrangler/src/api/integrations/platform/caches.ts
+type Cloudflare = Omit<PlatformProxy<Env>, "dispose" | "caches"> & {
+  caches: CacheStorage;
+};
+
 declare module "@remix-run/cloudflare" {
   interface Future {
     v3_singleFetch: true;
+  }
+
+  interface AppLoadContext {
+    cloudflare: Cloudflare;
   }
 }
 
